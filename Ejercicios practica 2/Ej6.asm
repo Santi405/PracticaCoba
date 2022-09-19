d729f720576c7b0d3f31a926fa9e0df5e763a49e
Santino Zanin <69124536+Santi405@users.noreply.github.com>
2022-09-19 03:40:51 -0300

    Create Ej6.asm

---
+++ Ejercicios practica 2/Ej6.asm
@@ -0,0 +1,31 @@
+        .data
+V:      .byte 43,26,26,5,18,10,29,31
+vLen:   .byte 7
+rango1: .byte 15
+rango2: .byte 30
+total:  .space 1
+        .text
+ 
+        lb $t2, rango1($0)
+        lb $t3, rango2($0)
+        lb $t4, vLen($0)
+        andi $t5, 0
+        andi $t6, 0
+        andi $t0, 0
+ 
+bucle:  bgt $t0, $t4, fin
+        lb $t5, V($t0)
+        bgt $t5, $t2, true1
+        add $t0, $t0, 1
+        j bucle
+ 
+true1:  
+        blt $t5, $t3, true2
+        add $t0, $t0, 1
+        j bucle
+ 
+true2:  add $t6, $t6, 1
+        add $t0, $t0, 1
+        j bucle
+ 
+fin:    sb $t6, total($0)
