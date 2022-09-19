6d02d66465c8e6c28298d7b2ed0a6f052ad8b537
Santino Zanin <69124536+Santi405@users.noreply.github.com>
2022-09-19 03:29:19 -0300

    Create Ej3.asm

---
+++ Ejercicios practica 2/Ej3.asm
@@ -0,0 +1,22 @@
+        .data
+V:      .byte 10,-4,-5,-2
+res:    .word 1
+        .text
+ 
+        la $t2, res
+        la $t5, 3
+        andi $t0, 0
+ 
+bucle:      
+        bgt $t0, $t5, fin
+        lb, $t3, V($t0)
+        bgtz $t3, casoPositivo
+        addi $t0, $t0, 1
+        j bucle
+ 
+casoPositivo:
+        andi $t2, 0
+ 
+fin:
+        sne $t2,$t2,$0
+        sb $t2, res($0)
